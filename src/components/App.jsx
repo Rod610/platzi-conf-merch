b591e0aff065098686ae0dab3add01e54a67f9be
Rodrigo Martinez <rodrigomartinez@Rodrigos-MacBook-Pro.local>
2022-09-23 15:27:45 -0500

    [add] firebase config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/components/App.css';
 
 const App = () =>{
-    return (<h1>Hola Mundo</h1>);
+    return (<h1>Plati Store Merch</h1>);
 }
 
 export default App;
