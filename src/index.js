fae2b38442e70b5a457a190f95821023fff22305
Rodrigo Martinez <rodrigomartinez@Rodrigos-MacBook-Pro.local>
2022-09-23 15:37:48 -0500

    [add] fixing depencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './routes/App';
-import './styles/components/app.css';
+import './styles/components/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
